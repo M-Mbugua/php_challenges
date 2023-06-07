891938a2a47d6d91147f3749d9829a3866dc1cb7
M-Mbugua <mbuguamichelle9@gmail.com>
2023-06-07 13:22:49 +0000

    Initial commit

---
+++ Typing Game/test.php
@@ -0,0 +1,10 @@
+class TypingGame extends TestCase
+{
+	public function tests()
+	{
+		$this->assertSame([1, 1, -1], correctStream(['it', 'is', 'find'], ['it', 'is', 'fine']));
+		$this->assertSame([1, -1, 1, 1, 1], correctStream(['april', 'showrs', 'bring', 'may', 'flowers'], ['april', 'showers', 'bring', 'may', 'flowers']));
+		$this->assertSame([1, -1, 1], correctStream(['weird', 'indicr', 'moment'], ['weird', 'indict', 'moment']));
+		$this->assertSame([1, 1, 1], correctStream(['starry', 'wind', 'skies'], ['starry', 'wind', 'skies']));
+	}
+}
---
+++ Typing Game/typing_game.php
@@ -0,0 +1,5 @@
+function correctStream($user, $correct) {
+	return array_map(function($x, $y) {
+		return $x==$y ? 1 : -1;
+	}, $user, $correct);
+}
